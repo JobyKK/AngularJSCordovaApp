a277c5e64c04becef85cca0b6b91cd658e207c38
JobyKK <jobyand121@gmail.com>
2014-10-04 22:30:46 +0300

    edit readme

--- platforms/android/assets/www/js/action.js
+++ platforms/android/assets/www/js/action.js
@@ -1,43 +1,102 @@
-$("document").ready(function() {
-	$("#menu").hover(function() {
-			document.getElementById("menu").setAttribute("src","img/button/menu_click.png");
-		}, function() {
-			document.getElementById("menu").setAttribute("src","img/button/menu.png");
-		});
-	$("#about").hover(function() {
-			document.getElementById("about").setAttribute("src","img/button/about_click.png");
-		}, function() {
-			document.getElementById("about").setAttribute("src","img/button/about.png");
-		});
-	$("#navi").hover(function() {
-			document.getElementById("navi").setAttribute("src","img/button/navi_click.png");
-		}, function() {
-			document.getElementById("navi").setAttribute("src","img/button/navi.png");
-		});
-	$("#code").hover(function() {
-			document.getElementById("code").setAttribute("src","img/button/code_click.png");
-		}, function() {
-			document.getElementById("code").setAttribute("src","img/button/code.png");
-		});
-	$("#foto").hover(function() {
-			document.getElementById("foto").setAttribute("src","img/button/foto_click.png");
-		}, function() {
-			document.getElementById("foto").setAttribute("src","img/button/foto.png");
-		});
+﻿$("document").change(function(){   
+    $("#add").hover(function() {
+            document.getElementById("add").setAttribute("src","img/button/add_click.png");
+        }, function() {
+            document.getElementById("add").setAttribute("src","img/button/add.png");
+        });
+    $("#menu").hover(function() {
+            document.getElementById("menu").setAttribute("src","img/button/menu_click.png");
+        }, function() {
+            document.getElementById("menu").setAttribute("src","img/button/menu.png");
+        });
+    $("#navi").hover(function() {
+            document.getElementById("navi").setAttribute("src","img/button/navi_click.png");
+        }, function() {
+            document.getElementById("navi").setAttribute("src","img/button/navi.png");
+        });
+    $("#code").hover(function() {
+            document.getElementById("code").setAttribute("src","img/button/code_click.png");
+        }, function() {
+            document.getElementById("code").setAttribute("src","img/button/code.png");
+        });
+    $("#foto").hover(function() {
+            document.getElementById("foto").setAttribute("src","img/button/foto_click.png");
+        }, function() {
+            document.getElementById("foto").setAttribute("src","img/button/foto.png");
+        });
 });
-
-
-function clic(id,name) {	
-	getXML
+$("document").ready(function(){
+    $("#add").hover(function() {
+            document.getElementById("add").setAttribute("src","img/button/add_click.png");
+        }, function() {
+            document.getElementById("add").setAttribute("src","img/button/add.png");
+        });
+    $("#menu").hover(function() {
+            document.getElementById("menu").setAttribute("src","img/button/menu_click.png");
+        }, function() {
+            document.getElementById("menu").setAttribute("src","img/button/menu.png");
+        });
+    $("#navi").hover(function() {
+            document.getElementById("navi").setAttribute("src","img/button/navi_click.png");
+        }, function() {
+            document.getElementById("navi").setAttribute("src","img/button/navi.png");
+        });
+    $("#code").hover(function() {
+            document.getElementById("code").setAttribute("src","img/button/code_click.png");
+        }, function() {
+            document.getElementById("code").setAttribute("src","img/button/code.png");
+        });
+    $("#foto").hover(function() {
+            document.getElementById("foto").setAttribute("src","img/button/foto_click.png");
+        }, function() {
+            document.getElementById("foto").setAttribute("src","img/button/foto.png");
+        });
+});
+function clic(id) {
+     
+    var main = document.getElementById("main");
+    var x = getXML(document.getElementById("inp").value);
+    main.innerHTML = x;
 }
-
-
+ 
+ 
 function myFunction(json) {
-	alert(json[0].id);	
+    alert(json[0].id); 
 }
-
-function getXML() {
-	var IDR = document.getElementById("inp").value;
-	alert("push push get XML");
-}
-
+ 
+function getXML(value) {
+    var IDR = document.getElementById("inp").value;
+    var jso = JSON.parse(xmlf);
+    var data = [];
+    for(i = 0; i < jso.length; i++) {
+        if (jso[i].code == value) {
+            data.push(jso[i].combined);
+            data.push(jso[i].confirmation_state);
+            data.push(jso[i].sbj_state);
+            data.push(jso[i].qvd);
+            data.push(jso[i].tel);
+            data.push(jso[i].name);
+            if (jso[i].sbj_state.indexOf("в стані припинення") != -1) {
+                data.push("yellow.png");
+            } else if (jso[i].sbj_state.indexOf("не перебуває в процесі припинення") != -1){
+                data.push("green.png");
+            } else {
+                data.push("red.png");
+            }
+        }
+    }
+     
+ 
+    if (data[0] != undefined) {
+        var text = '<img src="img/button/'+data[6]+'" style="margin-top:5px;"><br><h4>'+data[0]+'</h4><h5>'+data[1]+'</h5><h5>Стан підприємства: '+data[2]+'</h5><h5>Типи діяльності: '+data[3]+'</h5><h5>Тел:'+data[4]+'</h5><img src="img/button/add.png" id="add" onclick="add(this)"><br><img src="img/button/menu.png" id="menu" onclick="menus()">';
+    } else {
+        var text = '<h4>Нічого не знайдено</h4><img src="img/button/menu.png" id="menu" onclick="menus()">';
+    }
+    return '<div class="container" id="main">'+text+'</div>';
+};
+ 
+function menus() {
+    var main = document.getElementById("main");
+    main.innerHTML = '<h1 style="color:#008000;"> Введіть код</h1><form action="#" id="form"><input value="00000023" name="edr" style="margin-bottom:30px" id="inp"><br><img src="img/button/code.png" id="code" onclick="clic(this)" style="margin-bottom:30px"><br></form><img src="img/button/foto.png" id="foto" onclick="clic(this)">';
+ 
+} 
--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -3,10 +3,10 @@ window.addEventListener('load', function () {
 }, false);
 var page1 =     
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Back</a><h1>Menu</h1></div>' +
         '<div class="scroller">' +
-            '<div class="robot" id="main">' +
-'<link href="css/bootstrap.css" rel="stylesheet"><link href="css/bootstrap-responsive.css" rel="stylesheet"><link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png"><link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png"><link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png"><link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png"><link rel="shortcut icon" href="img/favicon.png"><link href="css/style.css" rel="stylesheet"><link href="css/app.css" rel="stylesheet"><script src="js/action.js"></script><div class = "navig"><img src="img/button/about.png" id="about" onclick="clic(this)">	<img src="img/button/menu.png" id="menu" onclick="clic(this)"><img src="img/button/navi.png" id="navi" onclick="clic(this)"></div><div class="container"><h1 style="color:#e4e56a;"> Введіть код</h1><form action="#" id="formm"><input value="12345678" name="edr" style="margin-bottom:30px" id="inp"><br><img src="img/button/code.png" id="code" onclick="clic(this)" style="margin-bottom:30px"><br></form><img src="img/button/foto.png" id="foto" onclick="clic(this)"></div>'
+            '<div class="robot" >' +
+'<link href="css/bootstrap.css" rel="stylesheet"><link href="css/bootstrap-responsive.css" rel="stylesheet"><link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/apple-touch-icon-144-precomposed.png"><link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114-precomposed.png"><link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/apple-touch-icon-72-precomposed.png"><link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-57-precomposed.png"><link rel="shortcut icon" href="img/favicon.png"><link href="css/style.css" rel="stylesheet"><link href="css/app.css" rel="stylesheet"><script src="js/jsonn.js"></script><script src="js/action.js"></script><div class="container" id="main"><h1 style="color:green;"> Enter code</h1><form action="#" id="formm"><input value="00000007" name="edr" style="margin-bottom:30px" id="inp"><br><img src="img/button/code.png" id="code" onclick="clic(this)" style="margin-bottom:30px"><br></form><img src="img/button/foto.png" id="foto" onclick="clic(this)"></div>'
 
 		+'</div>' +
         '</div>' +
@@ -14,39 +14,53 @@ var page1 =
 var tmp = "document.location.reload(true);"
 var page2 =     
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Back</a><h1>Map</h1></div>' +
         '<div class="scroller">' +
             '<div class="robot">' +
                 '<div id="panel"><input id="address" type="textbox" value="Kiev">'+
-    		'<input type="button" value="Адреса" onclick="codeAddress()"><input type="button" value="Обновить" onclick="document.location.reload(true)"></div><div id="map-canvas"></div>'+
+    		'<input type="button" value="Address" onclick="codeAddress()"><input type="button" value="Refresh" onclick="document.location.reload(true)"></div><div id="map-canvas"></div>'+
             '</div>' +
         '</div>' +
     '</div>';
 var page3 =     
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Back</a><h1>About us</h1></div>' +
         '<div class="scroller">' +
-            '<div class="robot">' +
-                '/*Your data sample*/'+
+            '<div class="robot" style="text-align:  center;">' +
+                '<p>Єдиний державний реєстр юридичних осіб та фізичних осіб - підприємців (далі - Єдиний державний реєстр) - автоматизована система збирання, накопичення, захисту, обліку та надання інформації про юридичних осіб та фізичних осіб – підприємців. (Закон України "Про державну реєстрацію юридичних осіб та фізичних осіб-підприємців").</p>'+
+		'<h1>Прес-служба</h1>'+
+		'<p>(044) 233-64-71, 233-66-81 (для засобів масової інформації)</p>'+
+		'<p>press@drsu.gov.ua</p>'+
+		'<h1>Діловод Департаменту</h1>'+
+		'<p>(044) 233-64-87</p>'+
+		'<h2>Телефон для довідок з питань державної реєстрації речових прав на нерухоме майно</h2>'+
+		'<p>(044) 233-64-78</p>'+
+		'<h1>Офіс ДП "ІРЦ":</h1>'+
+		'<p>    Телефон/факс: (044) 481-49-20</p>'+
+    		'<p>E-mail: office@irc.gov.ua</p>'+
+		'<h1>Служба технічної підтримки ДП "ІРЦ":</h1>'+
+		'<p>Телефон/факс: (044) 481-49-27</p>'+
+		'<p>E-mail: support@irc.gov.ua</p>'+
             '</div>' +
         '</div>' +
     '</div>';
 // The dynamically built HTML pages. In a real-life app, In a real-life app, use Handlerbar.js, Mustache.js or another template engine
 var homePage =
     '<div>' +
-        '<div class="header"><h1>Page Slider</h1></div>' +
+        '<div class="header"><h1>Person checker</h1></div>' +
         '<div class="scroller">' +
-            '<ul class="list">' +
-                '<li><a href="#page1"><strong>Build Bot</strong></a></li>' +
-                '<li><a data-no-turbolink="true" href="#page2"><strong>Medi Bot</strong></a></li>' +
-                '<li><a href="#page3"><strong>Ripple Bot</strong></a></li>' +
+            '<ul class="list" style="text-align:  center;">' +
+                '<li><a href="#page1"><strong>Menu</strong></a></li>' +
+                '<li><a data-no-turbolink="true" href="#page2"><strong>Map</strong></a></li>' +
+                '<li><a href="#page3"><strong>About us</strong></a></li>' +
+                '<li><a href="contact.html"><strong>Contacts</strong></a></li>' +
             '</ul>' +
         '</div>' +
     '</div>';
 
 var detailsPage =
     '<div>' +
-        '<div class="header"><a href="#" class="btn">Back</a><h1>Robot</h1></div>' +
+        '<div class="header"><a href="#" class="btn">Back</a></div>' +
         '<div class="scroller">' +
             '<div class="robot">' +
                 '/*Your data sample*/'
@@ -69,7 +83,8 @@ function route(event) {
         page = page2;
     } else if (hash === "#page3") {
         page = page3;
-    }
+    } else if (hash === "#page4") {
+        page = page4;}
     else {
         page = homePage;
 //        slider.slide($(homePage), "left");
